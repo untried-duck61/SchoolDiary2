a4d82d63a9e45d6737ae869c00dbea61663599db
Ivan-Al_20 <ialeshechkin0@gmail.com>
2026-02-23 16:13:21 +0400

    Add log import

--- app/src/main/java/ru/untriedduck/schooldiary2/LoginActivity.kt
+++ app/src/main/java/ru/untriedduck/schooldiary2/LoginActivity.kt
@@ -2,6 +2,7 @@ package ru.untriedduck.schooldiary2
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
