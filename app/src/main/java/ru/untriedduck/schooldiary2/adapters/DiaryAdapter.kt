61436886f2e3bd7ef267d4b627651565ac5de231
Ivan Aleshechkin <118288411+untried-duck61@users.noreply.github.com>
2026-03-17 10:30:07 +0400

    Make lesson display nothing if there is no homework

--- app/src/main/java/ru/untriedduck/schooldiary2/adapters/DiaryAdapter.kt
+++ app/src/main/java/ru/untriedduck/schooldiary2/adapters/DiaryAdapter.kt
@@ -51,7 +51,7 @@ class DiaryAdapter(private var lessons: List<Lesson>) : RecyclerView.Adapter<Dia
             val allAssignments = lesson.assignments ?: emptyList()
             // 1. Ищем только Домашнее Задание (typeId == 3)
             val homework = allAssignments.find { it.typeId == 3 }
-            tvAssignment.text = homework?.assignmentName ?: "Нет задания"
+            tvAssignment.text = homework?.assignmentName ?: ""
 
             val marks = allAssignments.mapNotNull { it.mark?.markValue }.filter { it.isNotEmpty() }
 
