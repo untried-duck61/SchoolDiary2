11a451142b11e3f5bc303189ba2e700cfc1d300a
Ivan Aleshechkin <118288411+untried-duck61@users.noreply.github.com>
2026-04-01 11:36:41 +0400

    Fix code readability issues

--- app/src/main/java/ru/untriedduck/schooldiary2/api/Attachment.kt
+++ app/src/main/java/ru/untriedduck/schooldiary2/api/Attachment.kt
@@ -2,5 +2,6 @@ package ru.untriedduck.schooldiary2.api
 
 data class Attachment(
     val id: Int,
-    val fileName: String,    val attachmentTypeId: Int
+    val fileName: String,
+    val attachmentTypeId: Int
 )
